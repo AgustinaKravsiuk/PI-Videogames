63299b8d167e84225ce3987c38ca4ae2e1dfb6e6
AgustinaKravsiuk <agustinakravsiuk@gmail.com>
2023-05-10 10:48:14 -0300

    Modelos Listos

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -3,10 +3,33 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define('Videogame', {
+    id:{
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
     },
   });
 };
