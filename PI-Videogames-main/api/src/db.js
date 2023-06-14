5f70e01b2b87dbbd067d5db7566ecd14d7b66b97
AgustinaKravsiuk <agustinakravsiuk@gmail.com>
2023-06-14 18:35:16 -0300

    correccion

--- PI-Videogames-main/api/src/db.js
+++ PI-Videogames-main/api/src/db.js
@@ -9,11 +9,7 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-    ssl: {
-      require: true
-    }
-  }
+  dialectOptions: {}
 });
 const basename = path.basename(__filename);
 
